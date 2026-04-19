00c161e9dbb3439393583ad663d28cf4b5d57e97
MortyStream <mxrtystream@gmail.com>
2026-04-20 01:45:03 +0200

    Update task.ts

--- expo/types/task.ts
+++ expo/types/task.ts
@@ -1,4 +1,5 @@
 import { User } from './user';
+import React, { useState, useRef, useEffect } from 'react';
 
 export type TaskPriority = 'high' | 'medium' | 'low';
 export type TaskStatus = 'pending' | 'in_progress' | 'completed' | 'validated';

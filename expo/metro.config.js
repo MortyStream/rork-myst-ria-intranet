dd3bc65239cd1bf7d811ded584951894b046c4f6
MortyStream <mxrtystream@gmail.com>
2026-04-19 14:43:57 +0200

    Update metro.config.js

--- expo/metro.config.js
+++ expo/metro.config.js
@@ -2,8 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+const emptyModules = ['stream', 'events', 'https', 'http', 'net', 'tls', 'fs', 'crypto'];
+
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName === 'stream') {
+  if (emptyModules.includes(moduleName)) {
     return { type: 'empty' };
   }
   return context.resolveRequest(context, moduleName, platform);
